b4ec9cc49fa64316fd29339c4e5f7a214a0255bb
Austin1serb <serbaustin@gmail.com>
2025-06-11 00:19:48 -0700

    added cli script

--- src/cli/init.cjs
+++ src/cli/init.cjs
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
 const { runZeroUiInit } = require('./postInstall.cjs');
-runZeroUiInit();
+
+module.exports = function cli(argv = process.argv.slice(2)) {
+  runZeroUiInit(argv);
+
+  // still allow direct execution:
+  if (require.main === module) process.exit(0);
+};
