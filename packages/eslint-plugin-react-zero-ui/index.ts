f069f5a4f53e091c441f2a9dd904957b78c58ad7
Austin1serb <serbaustin@gmail.com>
2025-08-01 00:52:21 -0700

    working eslint plugin

--- packages/eslint-plugin-react-zero-ui/index.ts
+++
@@ -1,6 +0,0 @@
-import requireDataAttr from './src/rules/require-data-attr.js';
-
-export const rules = { 'require-data-attr': requireDataAttr } as const;
-
-// recommended config for users
-export const configs = { recommended: { plugins: ['react-zero-ui'], rules: { 'react-zero-ui/require-data-attr': 'error' } } } as const;
