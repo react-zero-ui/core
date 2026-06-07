3d0e3a1061aa4a06a2cbb14e6d14b65f7f2045c2
Austin1serb <serbaustin@gmail.com>
2026-06-07 15:33:18 -0600

    add redirect

--- examples/demo/next.config.mjs
+++ examples/demo/next.config.mjs
@@ -1,28 +1,21 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { createMDX } from 'fumadocs-mdx/next';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { createMDX } from "fumadocs-mdx/next";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  reactStrictMode: true,
-  outputFileTracingRoot: path.join(__dirname, '../..'),
-  async redirects() {
-    return [
-      {
-        source: '/react',
-        destination: '/demo/real-world',
-        permanent: true,
-      },
-      {
-        source: '/zero-ui',
-        destination: '/demo/real-world',
-        permanent: true,
-      },
-    ];
-  },
+	reactStrictMode: true,
+	outputFileTracingRoot: path.join(__dirname, "../.."),
+	async redirects() {
+		return [
+			{ source: "/react", destination: "/demo/real-world", permanent: true },
+			{ source: "/zero-ui", destination: "/demo/real-world", permanent: true },
+			{ source: "/lucide-react", destination: "/icon-sprite", permanent: true },
+		];
+	},
 };
 
 export default withMDX(config);
